f7ef5dccf53c49f5521139fc8ea3272d51ddd634
bin <113090944+VTICCHR@users.noreply.github.com>
2023-11-30 13:05:53 +0900

    Title: 점수 입출력, Time: 122ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231130/점수 입출력/enter-int-and-print-score.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a;
+        a = sc.nextInt();
+        System.out.printf("Your score is %d point.", a);
+        // 여기에 코드를 작성해주세요.
+    }
+}
