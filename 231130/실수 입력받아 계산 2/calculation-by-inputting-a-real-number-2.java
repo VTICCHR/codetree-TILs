1c6bd4d2447e4053668692da2caada83c6272263
bin <113090944+VTICCHR@users.noreply.github.com>
2023-11-30 13:20:48 +0900

    Title: 실수 입력받아 계산 2, Time: 136ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 231130/실수 입력받아 계산 2/calculation-by-inputting-a-real-number-2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        double a, b;
+        a = sc.nextDouble();
+        b = a+1.5;
+        System.out.printf("%.2f", b);
+
+    }
+}
